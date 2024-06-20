b8789ed56d9b4fea994b4ca3f5a129234e5dd8a3
ProgSomel <somelahmed88@gmail.com>
2024-06-20 15:18:32 +0600

    pages

---
+++ svelteKit/joy-of-code/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { onMount, onDestroy } from "svelte";
+
+    onMount(()=> console.log('Mounted Home page'))
+    onDestroy(()=> console.log('Unmounted Home page'))
+</script>
+
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/posts">Posts</a>
+        </li>
+    </ul>
+</nav>
+<h1 class="text-2xl font-bold">This is Home Page</h1>
+<style>
+    h1 {
+        color: red;
+    }
+</style>
---
+++ svelteKit/joy-of-code/src/routes/posts/+page.svelte
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li>
+            <a href="/">Home</a>
+        </li>
+        <li>
+            <a href="/posts">Posts</a>
+        </li>
+    </ul>
+</nav>
+<h1 class="text-2xl font-bold">This is Posts Page</h1>
