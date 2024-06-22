430ab29b3d9594e7884cc8420752c572db1903be
ProgSomel <somelahmed88@gmail.com>
2024-06-22 14:10:06 +0600

    folder updated

---
+++ svelteKit/svelte.dev/src/routes/Reactivity/+page.svelte
@@ -0,0 +1,23 @@
+<nav>
+    <ul>
+        <li>
+            <a href="../Reactivity/reactiveAssignments/">Reactive assignments</a>
+        </li>
+        <li>
+            <a href="../Reactivity/reactiveDeclaration/">Reactive declartions</a>
+        </li>
+        <li>
+            <a href="../Reactivity/reactiveStatements/">Reactive Statements</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: center;
+    }
+    ul li {
+        font-size: 30px;
+    }
+</style>
---
+++ svelteKit/svelte.dev/src/routes/Reactivity/reactiveAssignments/+page.svelte
@@ -0,0 +1,29 @@
+<h1>Reactivity</h1>
+<script>
+    let count = 0;
+    function handleIncrement() {
+        count++;
+    }
+
+
+</script>
+
+<div>
+    <h4>Reactive assignments:</h4>
+<button on:click={handleIncrement}>
+    Clicked {count}
+    {count > 0 ?'times':'time' }
+</button>
+
+
+
+
+
+</div>
+
+
+<style>
+    div {
+        text-align: center;
+    }
+</style>
