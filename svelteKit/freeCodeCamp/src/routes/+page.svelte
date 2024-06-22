95a5abfefcdd71702f304a583a8b4d1f7670134b
ProgSomel <somelahmed88@gmail.com>
2024-06-22 14:02:34 +0600

    Reactivity Done

---
+++ svelteKit/freeCodeCamp/src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<nav>
+    <ul>
+        <li>
+            <a href="Reactivity">Reactivity</a>
+        </li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        display: flex;
+        justify-content: center;
+    }
+    ul li {
+        font-size: 34px;
+    }
+</style>
---
+++ svelteKit/freeCodeCamp/src/routes/Reactivity/+page.svelte
@@ -0,0 +1,13 @@
+<nav>
+    <ul>
+        <li>
+            <a href="../Reactivity/reactiveAssignments/">Reactive assignments</a>
+        </li>
+        <li>
+            <a href="../Reactivity/reactiveDeclaration/">Reactive declartions</a>
+        </li>
+        <li>
+            <a href="../Reactivity/reactiveStatements/">Reactive Statements</a>
+        </li>
+    </ul>
+</nav>
